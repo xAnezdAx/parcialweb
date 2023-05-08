a491cb40a1172eab139929982b4913d5f6624e6d
xAnezdAx <107273646+xAnezdAx@users.noreply.github.com>
2023-05-08 11:47:40 -0500

    archivos del proyecto descargado
    no tiene ningun cambio responto al entregado

---
+++ app/Http/Controllers/SalesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class SalesController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+
+    public function show()
+    {
+        
+    }
+
+    public function edit()
+    {
+        
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
+
+}
---
+++ database/migrations/2023_05_07_192636_create_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre_empleado");
+            $table->string("nombre_cliente");
+            $table->string("precio");
+            $table->integer("fecha_compra");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
